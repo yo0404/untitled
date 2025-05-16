97112ff75217e8d7989184f4dab74cec15494a38
241012 <241012@std.hi-joho.ac.jp>
2025-05-16 12:27:37 +0900

    課題

---
+++ src/Review4.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Review4{
+    public static void main(String[] args) {
+        int number = 5;
+        String str = 88 + "88";
+        double PI = 3.141592;
+        PI = 3.14;
+        double cric = 3.14;
+        int a = 10000000000;
+        boolen flg = "true";
+    }
+}
